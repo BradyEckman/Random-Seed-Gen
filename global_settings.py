5a5d0c19710af8e0d5e18ee5df67239b010e340c
BradyEckman <a02249268@usu.edu>
2022-06-08 15:15:26 -0600

    Full Functional

--- global_settings.py
+++ global_settings.py
@@ -1,4 +1,3 @@
 
 
-rerun = True
-
+rerun = False
--- start_execution.py
+++ start_execution.py
@@ -1,15 +1,15 @@
 import random
 import sys
+import os
 import global_settings as gs
 import test1
 import test2
-
 
 # seed = random.randrange(sys.maxsize)
 # file = open("seed.txt" , "w")
 # file.write(str(seed))
 
-if gs.rerun:
+if gs.rerun and os.path.exists("seed.txt"):
     file = open("seed.txt")
     lines = file.read().splitlines()[0]
     seed = int(lines)
@@ -19,6 +19,9 @@ else:
     file = open("seed.txt", "w")
     file.write(str(new_seed))
     random.seed(new_seed)
+
+
+
 
 test1.test1()
 test2.test2()
